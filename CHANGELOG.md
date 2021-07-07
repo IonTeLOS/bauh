084be48dec29e5e4131cb0a8f090c7ce01059fab
ion <iontelos@protonlinux.com>
2021-07-07 20:50:17 +0300

    rename

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,3 @@
 All notable changes to this project will be documented in this file.
 
 1.0 testing initial release
-1.1 minor changes
--- waffles/__init__.py
+++ waffles/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '2.0'
-__app_name__ = 'waffles'
+__version__ = '1.0'
+__app_name__ = 'Waffles'
 
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
