cbc9e5b5d31b59eeef94f81b2248f4ca0d308151
ion <teloslinux@protonmail.com>
2021-07-13 20:31:12 +0300

    identation fix

--- waffles/gems/web/controller.py
+++ waffles/gems/web/controller.py
@@ -10,6 +10,7 @@ from math import floor
 from pathlib import Path
 from threading import Thread
 from typing import List, Type, Set, Tuple, Optional
+from os.path import commonprefix
 
 import requests
 import yaml
@@ -774,8 +775,24 @@ class WebApplicationManager(SoftwareManager):
 
         return TransactionResult(success=True, installed=[pkg], removed=[])
 
+    def ql(s, eol=True):
+        lines = s.splitlines()
+        l0 = None
+        if lines:
+            l0 = lines.pop(0) or None
+        common = commonprefix(lines)
+        indent = re.match(r'\s*', common)[0]
+        n = len(indent)
+        lines2 = [l[n:] for l in lines]
+        if not eol and lines2 and not lines2[-1]:
+            lines2.pop()
+        if l0 is not None:
+            lines2.insert(0, l0)
+        s2 = "\n".join(lines2)
+        return s2
+
     def _gen_desktop_entry_content(self, pkg: WebApplication) -> str:
-        return """
+        return (ql("""
         [Desktop Entry]
         Type=Application
         Name={name} (web)
@@ -784,7 +801,7 @@ class WebApplicationManager(SoftwareManager):
         Exec={exec_path}
         {categories}
         {wmclass}
-        """.format(name=pkg.name, exec_path=pkg.get_command(),
+        """)).format(name=pkg.name, exec_path=pkg.get_command(),
                    desc=pkg.description or pkg.url, icon=pkg.get_disk_icon_path(),
                    categories='Categories={}'.format(';'.join(pkg.categories)) if pkg.categories else '',
                    wmclass="StartupWMClass={}".format(pkg.package_name) if pkg.package_name else '')
