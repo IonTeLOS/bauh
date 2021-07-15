926de492a8c22503a2d1cdde1ef989cc619530a4
iontelos <teloslinux@protonmail.com>
2021-07-15 08:53:58 +0300

    test

--- waffles/api/constants.py
+++ waffles/api/constants.py
@@ -4,5 +4,5 @@ from pathlib import Path
 CACHE_PATH = '{}/.cache/waffles'.format(str(Path.home()))
 CONFIG_PATH = '{}/.config/waffles'.format(str(Path.home()))
 USER_THEMES_PATH = '{}/.local/share/waffles/themes'.format(str(Path.home()))
-DESKTOP_ENTRIES_DIR = '{}/.local/share/applications'.format(str(Path.home()))
+DESKTOP_ENTRIES_DIR = '/usr/share/applications/waffles'.format(str(Path.home()))
 TEMP_DIR = '/tmp/waffles{}'.format('_root' if os.getuid() == 0 else '')
